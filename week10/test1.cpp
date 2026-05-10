ccea2529fcac0b9f73326bea6e225a9f9413d4de
hamin25 <shm25@handong.ac.kr>
2026-05-10 18:32:22 +0900

    add test1

--- week10/test1.cpp
+++ week10/test1.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main(){
-    
+    int n=0;
+    cin >> n;
+    int* num = (int*)malloc(sizeof(int)*n);
+    cin >> num[0];
+    int max = num[0];
+    int sum = num[0];
+    for(int i=1;i<n;i++){
+        cin >> num[i];
+        if(num[i]>max) max = num[i];
+        sum += num[i];
+    }
+    cout << sum << " " << max << endl;
+    free(num);
     return 0;
 }

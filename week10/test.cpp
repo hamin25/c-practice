b49165278d036c80cc51d2e7a8317791fe59250d
hamin25 <shm25@handong.ac.kr>
2026-05-17 23:52:13 +0900

    add pointer

--- week10/test.cpp
+++ week10/test.cpp
@@ -5,5 +5,10 @@ int main(){
     int a=0;
     cin >> a;
     cout << a << endl;
+    int* arr = new int[5];
+    for(int i=0;i<5;i++){
+        cin >> arr[i];
+    }
+    delete[] arr;
     return 0;
 }

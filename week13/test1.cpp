43a89dafdc39740165fea4449bff89b068ca396b
hamin25 <shm25@handong.ac.kr>
2026-05-20 23:16:36 +0900

    add empty to main

--- week13/test1.cpp
+++ week13/test1.cpp
@@ -35,6 +35,9 @@ public:
 int main() {
     Stack s;
 
+    if (s.isEmpty()) cout << "Stack is empty" << endl;
+    else cout << "Stack is not empty" << endl;
+
     s.push(10);
     s.push(20);
     s.push(30);

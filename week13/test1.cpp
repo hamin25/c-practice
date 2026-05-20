4dba0ab098c2620811f380ca0dab52eb837a0065
hamin25 <shm25@handong.ac.kr>
2026-05-20 23:15:07 +0900

    add stack

--- week13/test1.cpp
+++ week13/test1.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+
+class Stack {
+private:
+    int arr[100];
+    int top;
+
+public:
+    Stack() {
+        top = -1;
+    }
+
+    void push(int value) {
+        arr[++top] = value;
+    }
+
+    void pop() {
+        if (top >= 0)
+            top--;
+    }
+
+    void print() {
+        for (int i = 0; i <= top; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
 int main(){
+    
     return 0;
 }

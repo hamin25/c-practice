2bb07f2693f84b2678aaec3a777c72e997ea121a
hamin25 <shm25@handong.ac.kr>
2026-05-20 23:15:26 +0900

    add main

--- week13/test1.cpp
+++ week13/test1.cpp
@@ -29,7 +29,18 @@ public:
     }
 };
 
-int main(){
-    
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    s.print();
+
+    s.pop();
+
+    s.print();
+
     return 0;
 }

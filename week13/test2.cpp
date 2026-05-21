4b4d8fb1d09da97e07fe15a8472894e216b410e5
hamin25 <shm25@handong.ac.kr>
2026-05-21 21:23:32 +0900

    add enqueue function

--- week13/test2.cpp
+++ week13/test2.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
 using namespace std;
 
-
-class Queue {
+class Queue
+{
 private:
-    int* arr;
+    int *arr;
     int frontIndex;
     int rearIndex;
     int capacity;
 
 public:
-    Queue(int size) {
+    Queue(int size)
+    {
         capacity = size;
         arr = new int[capacity];
         frontIndex = 0;
         rearIndex = 0;
     }
+    void enqueue(int value)
+    {
+        arr[rearIndex] = value;
+        rearIndex++;
+    }
 };
 
+int main()
+{
+    Queue q(10);
 
-int main(){
-    
+    q.enqueue(1);
+    q.enqueue(2);
     return 0;
 }

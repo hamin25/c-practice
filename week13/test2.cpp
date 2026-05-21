4c01bc23c55179034a6e12d179f7cefd9b8db2dc
hamin25 <shm25@handong.ac.kr>
2026-05-21 21:22:06 +0900

    add que class

--- week13/test2.cpp
+++ week13/test2.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+
+class Queue {
+private:
+    int* arr;
+    int frontIndex;
+    int rearIndex;
+    int capacity;
+
+public:
+    Queue(int size) {
+        capacity = size;
+        arr = new int[capacity];
+        frontIndex = 0;
+        rearIndex = 0;
+    }
+};
+
+
 int main(){
     
     return 0;

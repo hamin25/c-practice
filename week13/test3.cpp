818b5d0ff618a037b361a23c55344b2ee43353e5
hamin25 <shm25@handong.ac.kr>
2026-05-28 23:23:36 +0900

    Add BinaryTree class and insert function

--- week13/test3.cpp
+++ week13/test3.cpp
@@ -13,7 +13,42 @@ struct Node{
     }
 };
 
+class BinaryTree{
+private:
+    Node* root;
+
+    Node* insert(Node* node, int value){
+        if(node == nullptr){
+            return new Node(value);
+        }
+
+        if(value < node->data){
+            node->left = insert(node->left, value);
+        }
+        else{
+            node->right = insert(node->right, value);
+        }
+
+        return node;
+    }
+
+public:
+    BinaryTree(){
+        root = nullptr;
+    }
+
+    void insert(int value){
+        root = insert(root, value);
+    }
+};
+
 int main(){
+
+    BinaryTree tree;
+
+    tree.insert(10);
+    tree.insert(5);
+    tree.insert(20);
 
     return 0;
 }

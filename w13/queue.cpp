1f63802e07c68429f7b918868b919e617f953d4c
hamin25 <shm25@handong.ac.kr>
2026-05-29 21:24:52 +0900

    add queue class

--- w13/queue.cpp
+++ w13/queue.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+class MyCirucularQueue{
+    int front;
+    int rear;
+    int size;
+    int count;
+    int *list;
+    public:
+        MyCirucularQueue(){};
+        ~MyCirucularQueue(){};
+        void dequeue();
+        void enqueue(int v);
+        bool isEmpty() const;
+        bool isFull() const;
+        int getFront() const;
+        int getRear() const;
+        void clear();
+        void print() const;
+        int getSize() const;
+};
+
+
+
 int main(){
-    
+
     return 0;
 }

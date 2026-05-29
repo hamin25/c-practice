c9558b7d270b76b8eb658cae0acee7025b809f14
hamin25 <shm25@handong.ac.kr>
2026-05-29 21:30:23 +0900

    add queue method

--- w13/queue.cpp
+++ w13/queue.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class MyCirucularQueue{
+class MyCircucularQueue{
     int front;
     int rear;
-    int size;
+    int maxsize;
     int count;
     int *list;
     public:
-        MyCirucularQueue(){};
-        ~MyCirucularQueue(){};
+        MyCircucularQueue(int size) : count(0), front(-1), rear(-1) {
+            if(count<=0) count=1;
+            list = new int[size];
+            maxsize = size;
+        }
+        ~MyCircucularQueue(){
+            delete [] list;
+        }
         void dequeue();
         void enqueue(int v);
         bool isEmpty() const;
@@ -26,4 +32,54 @@ class MyCirucularQueue{
 int main(){
 
     return 0;
+}
+
+void MyCircularQueue::dequeue(){
+    if(isempty()) return;
+    front = (front +1) % maxsize;
+    count --;
+}
+
+void MyCircularQueue::enqueue(int v){
+    if(isfull()) return;
+    rear = (rear +1) % maxsize;
+    list[rear] = v;
+    if(front == -1) front =0;
+    count ++;
+
+}
+
+bool MyCircularQueue::isempty() const {
+    return (count==0);
+}
+
+bool MyCircularQueue::isfull() const {
+    return (count==maxsize);
+}
+
+int MyCircularQueue::getfront() const{
+    if(isempty()) return;
+    return list[front];
+}
+
+int MyCircularQueue::getrear() const{
+    if(isempty()) return;
+    return list[rear];
+}
+
+void MyCircularQueue::clear()
+{
+    front = rear = -1;
+    count = 0;
+}
+
+void MyCircularQueue::print() const{
+    for(int i=0;i<count;i++){
+        int idx = (front+i) % maxsize;
+        cout << list[idx] << endl;
+    }
+}
+
+int MyCircularQueue::getcount() const{
+    return count;
 }

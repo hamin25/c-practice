43342ee528f5421a8209f0ce2fe964955472b4cd
hamin25 <shm25@handong.ac.kr>
2026-06-04 23:22:53 +0900

    feat: create node and stack skeleton

---
+++ week14/LinkedListstack.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+class Stack {
+private:
+    Node* topNode;
+
+public:
+    Stack() {
+        topNode = nullptr;
+    }
+};
+
+int main() {
+    Stack s;
+
+    cout << "Stack created!" << endl;
+
+    return 0;
+}
